6a05bd79467e1fa035f4cbba8f41b2abb4816ce6
Anderson Macedo Cavalcanti <anderson.macedo.cavalcanti@gmail.com>
2021-02-16 00:18:18 -0300

    Como trabalhar com toLowerCase e toUpperCase

--- JAVASCRIPT/Unidade07-Strings.js
+++ JAVASCRIPT/Unidade07-Strings.js
@@ -1,3 +1,16 @@
-let frase = "Meu time fez um gol"
-let primeiraPalavra = frase.substr(0,2)
-console.log(primeiraPalavra)
+let frase1 = "henrique soares dos anjos"
+let frase2 = "BOA TARDE"
+let maiusculo = frase1.toLocaleUpperCase()
+let minusculo = frase2.toLocaleLowerCase()
+let primeiraLetraMaiuscula = frase1.split(" ")
+
+let frase =""
+for(let i = 0; i<primeiraLetraMaiuscula.length;i++){
+    if(primeiraLetraMaiuscula[i].length>3) {
+        frase = frase.concat(primeiraLetraMaiuscula[i].charAt(0).toLocaleUpperCase().concat(primeiraLetraMaiuscula[i].substr(1)), " ");
+    }else {frase = frase.concat(primeiraLetraMaiuscula[i]," ")}
+}
+
+console.log(frase)
+console.log(maiusculo)
+console.log(minusculo)
