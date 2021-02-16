cf843be6fef1268a50520faa592edd04dc03d3f7
Anderson Macedo Cavalcanti <anderson.macedo.cavalcanti@gmail.com>
2021-02-16 01:07:06 -0300

    Como trabalhar com Hora

--- JAVASCRIPT/Unidade08-Date.js
+++ JAVASCRIPT/Unidade08-Date.js
@@ -5,3 +5,7 @@ console.log(agora.getDate()+"/"+(agora.getMonth()>10?(agora.getMonth()+1):"0"+(a
 let aniversario = new Date(1997,7,2)
 
 console.log(aniversario.getDate())
+
+console.log((agora.getHours()<10?"0"+agora.getHours():agora.getHours())+
+    ":"+(agora.getMinutes()<10?"0"+agora.getMinutes():agora.getMinutes())+
+    ":"+agora.getMilliseconds());
