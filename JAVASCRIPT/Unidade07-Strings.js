c199806bf397374134ff697e97200c2c0114b450
Anderson Macedo Cavalcanti <anderson.macedo.cavalcanti@gmail.com>
2021-02-15 23:34:12 -0300

    Como trabalhar com charAt e split

---
+++ JAVASCRIPT/Unidade07-Strings.js
@@ -0,0 +1,6 @@
+let frase = "Meu time fez um gol"
+let separacao = frase.split(" ");
+let primeiraPalavra = separacao[0]
+let primeiraLetra = frase.charAt(0)
+console.log(primeiraPalavra)
+console.log(primeiraLetra)
