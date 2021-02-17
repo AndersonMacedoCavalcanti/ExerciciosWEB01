03335d29919ed8d50005d876b72fb217d88734e7
Anderson Macedo Cavalcanti <anderson.macedo.cavalcanti@gmail.com>
2021-02-17 19:23:26 -0300

    Projeto - Criar um Image Rotator - aula 4

--- jquery/myJqueryScripts/my_Jquery07.js
+++ jquery/myJqueryScripts/my_Jquery07.js
@@ -1,16 +1,30 @@
 $(function () {
 
+    var limite;
+
     function animar() {
+
         $("#janelaPrinc").animate({left:"-=200"},1000, function () {
 
-            if($("#janelaPrinc").position().left == -600){
+            if($("#janelaPrinc").position().left == limite){
                 $("#janelaPrinc").animate({left:"0"},0)
             }
         })
     }
 
     $(function () {
+
+        limite =$("#janelaPrinc img").length * $("#janelaPrinc img").width()*-1;
+
+        $("#janelaPrinc").append("<img>")
+        $("#janelaPrinc img:last").attr("src","../HTML/imagens/Pato-LAranja_bx2.jpg")
+
+
+
+
         setInterval(animar,2000);
+
+
 
     })
 
