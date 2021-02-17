7e477b3cf10b7372cdf90ebec4f7fa4d5048b19a
Anderson Macedo Cavalcanti <anderson.macedo.cavalcanti@gmail.com>
2021-02-17 18:51:27 -0300

    Projeto - Criar um Image Rotator - aula 3

--- jquery/myJqueryScripts/my_Jquery07.js
+++ jquery/myJqueryScripts/my_Jquery07.js
@@ -1,7 +1,12 @@
 $(function () {
 
     function animar() {
-        $("#janelaPrinc").animate({left:"-=200"},1000)
+        $("#janelaPrinc").animate({left:"-=200"},1000, function () {
+
+            if($("#janelaPrinc").position().left == -600){
+                $("#janelaPrinc").animate({left:"0"},0)
+            }
+        })
     }
 
     $(function () {
