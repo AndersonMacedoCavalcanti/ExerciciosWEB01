2df7c8ed463fb8cbab2567e63bacff142c41bc62
Anderson Macedo Cavalcanti <anderson.macedo.cavalcanti@gmail.com>
2021-02-17 15:32:38 -0300

    Mostrar e esconder elementos com clique / Mostrar e esconder elementos ao rolar o mouse por cima.

--- jquery/myJqueryScripts/my_Jquery06.js
+++ jquery/myJqueryScripts/my_Jquery06.js
@@ -1,21 +1,62 @@
 $(function () {
 
-    /*Primeira forma*/
-    $("#cursoHtml").click(function () {
-        alert("clicou em html");
+    /* ---------------------esconder e mostrar ao clicar------------------------*/
+
+    $(function () {
+
+        $("#mostrar").on("click",mostarPorMou)
+
+        $("#esconder").on("click",esconderPorMou)
+
     })
 
-    /*Segunda forma*/
-    $("#cursoCss").on("click", function () {
-        alert("clicou em css");
-    })
-
-    function mostrarMesagem() {
-        alert("Passou o mouse por cima");
-        $("#cursoCss").off("mouseover", mostrarMesagem) /*off mata o evento apos a execucao dele*/
-        /*deixar o off se, parametros ele mata tod o evento dele*/
+    function mostarPorMou() {
+        $("#janela").show()
     }
 
-    $("#cursoCss").on("mouseover",mostrarMesagem)
+    function esconderPorMou() {
+        $("#janela").hide()
+    }
+
+
+
+/* ---------------------esconder e mostrar ao passar o mouse------------------------*/
+
+    $(function () {
+
+        $("#janela").hover(esconder,mostrar) /*simplificacao de colocar o mouse por cima e tirar*/
+    })
+
+
+    function mostrar() {
+        $("#janela").show()
+    }
+
+    function esconder() {
+        $("#janela").hide()
+
+    }
+
+/*---------------------------------------------------------------------------------------------------------*/
+
+    $(function () {
+        $("main").css("width",$(window).width()-16).css("height",$(window).height()-16);
+
+
+    })
+
+        function f() {
+
+            let altura = $(window).height()-16
+            let largura = $(window).width()-16
+
+            $("main").css("width",largura).css("height",altura);
+        }
+
+        $(window).resize(function () {
+            f()
+        })
 })
+
+
 
