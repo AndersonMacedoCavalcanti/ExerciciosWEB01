d6551af25045b0f059195ad67e30b192d155ee84
Anderson Macedo Cavalcanti <anderson.macedo.cavalcanti@gmail.com>
2021-02-27 17:45:58 -0300

    Funções de Strings

--- PHP/Unidade07-Criandovariáveis.php
+++ PHP/Unidade07-Criandovariáveis.php
@@ -1,6 +1,5 @@
 <?php
-	$_marcar = 'Fiat';
-	$_nome = "Uno";
+	$_nome = "Curso PHP Fundamental";
 ?>
 
 
@@ -11,7 +10,25 @@
  </head>
  <body>
 	<?php
-		echo $_marcar .", ". $_nome;
-	?>
+		// strlen - Retorna a quantidade de letras
+		echo strlen ($_nome)." - ";
+
+		// stripos - Retorna primeira ocorrencia
+		echo stripos($_nome,"P")." - ";
+
+		// strripos - Retorna ultima ocorrencia
+        echo strripos($_nome,"P")." - ";
+
+        // strtolower - Converte para eltras mini
+		echo strtolower($_nome)." - ";
+
+		// strtolower - Converte para eltras Maius
+		echo strtoupper($_nome)." - ";
+
+		// SUBSTR_COUNT - Conta quantas ocorrencias
+		echo substr_count($_nome,"P")." - ";
+	    echo substr_count($_nome,"PHP")." - ";
+
+    ?>
  </body>
 </html>
