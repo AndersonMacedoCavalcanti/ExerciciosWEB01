9c11fb1574fb921b5dd4aae4b98643eb9a3e3850
Anderson Macedo Cavalcanti <anderson.macedo.cavalcanti@gmail.com>
2021-02-27 17:26:54 -0300

    Como criar suas primeiras variáveis em PHP

---
+++ PHP/Unidade07-Criandovariáveis.php
@@ -0,0 +1,15 @@
+<?php
+$_nomee = "Anderson Macedo Cavalcanti";
+$_salario = 3000;
+?>
+
+<html lang = "pt-BR">
+ <head>
+  <meta charset="UTF-8">
+  <title>Teste PHP</title>
+ </head>
+ <body>
+    <?php echo $_nomee;?>
+ <p><?php echo $_salario;?></p>
+ </body>
+</html>
