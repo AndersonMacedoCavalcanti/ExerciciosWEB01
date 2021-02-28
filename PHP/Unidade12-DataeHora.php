6b23aa382afb06568fbcc71871e161aa015ebd98
Anderson Macedo Cavalcanti <anderson.macedo.cavalcanti@gmail.com>
2021-02-28 13:32:19 -0300

    GetDate

---
+++ PHP/Unidade12-DataeHora.php
@@ -0,0 +1,14 @@
+
+<html lang = "pt-BR">
+ <head>
+  <meta charset="UTF-8">
+  <title>Unidade 7</title>
+ </head>
+ <body>
+	<?php
+	date_default_timezone_set("Brazil/East");
+	 $_agora = getdate();
+	print_r($_agora)
+    ?>
+ </body>
+</html>
