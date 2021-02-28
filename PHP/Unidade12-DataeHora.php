c7b8ab632d5c46c4c115b25e14ee8470c2751ac8
Anderson Macedo Cavalcanti <anderson.macedo.cavalcanti@gmail.com>
2021-02-28 13:47:07 -0300

    Mostrar Hora e Data

--- PHP/Unidade12-DataeHora.php
+++ PHP/Unidade12-DataeHora.php
@@ -3,12 +3,17 @@
  <head>
   <meta charset="UTF-8">
   <title>Unidade 7</title>
+
  </head>
  <body>
 	<?php
 	date_default_timezone_set("Brazil/East");
 	 $_agora = getdate();
-	print_r($_agora)
+	 $_hora = $_agora["hours"] . ":" . $_agora["minutes"]  . ":" . $_agora["seconds"] . "<br><br>";
+	 $_data = ($_agora["mday"]<10 ? "0" . $_agora["mday"] : $_agora["mday"]) . "/" . ($_agora["mon"]<10 ? "0" . $_agora["mon"] : $_agora["mon"])  . "/" . $_agora["year"] . "<br><br>";
+
+	 echo $_hora;
+	 echo $_data;
     ?>
  </body>
 </html>
