2ce8f012160e13fd6f17d883959690a2a81a3c38
Anderson Macedo Cavalcanti <anderson.macedo.cavalcanti@gmail.com>
2021-02-28 20:47:41 -0300

    Criar arquivo de conexão  separado

--- PHP - Banco de dados BASICO/ComocriarumaconexãocomMySQL.php
+++ PHP - Banco de dados BASICO/ComocriarumaconexãocomMySQL.php
@@ -1,15 +1,34 @@
+<?php
+
+	require_once("../PHP - Banco de dados BASICO/conexao/conexao.php");
+
+	if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+
+	$consultas_produtos = "select * from produto";
+	$produto = mysqli_query($conecta,$consultas_produtos);
+
+	if(!$produto){
+		die("falha na consulta");
+	}
+
+?>
+
+
 <html lang = "pt-BR">
  <head>
   <meta charset="UTF-8">
   <title>Unidade 7</title>
  </head>
  <body>
-    <?php require("utilitarios/curso3.php"); //caso o require encontre algum erro todu o processamento e parado ?>
+     <?php
+     while($registros = mysqli_fetch_assoc($produto)){ // pode usar assoc no lugar de row para mostrar como indice o nome das colunuas
+         print_r($registros);
+         echo "<br>";
 
-    <?php require("utilitarios/curso1.php"); ?>
-    "<br>"
-    <?php require("utilitarios/curso2.php"); ?>
-
+     }
+     ?>
  </body>
 </html>
 
