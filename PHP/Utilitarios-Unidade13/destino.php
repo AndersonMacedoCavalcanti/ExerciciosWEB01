a75cdf2bf997e2738dcb3fa955f43d552c480c71
Anderson Macedo Cavalcanti <anderson.macedo.cavalcanti@gmail.com>
2021-02-28 14:45:00 -0300

    Resgatar com parâmetros

--- PHP/Utilitarios-Unidade13/destino.php
+++ PHP/Utilitarios-Unidade13/destino.php
@@ -4,7 +4,14 @@
         <meta charset="UTF-8">
         <title>Curso PHP FUNDAMENTAL</title>
     </head>
+    <body>
 
-    <body> 
+        <?php
+            $_codig = $_GET["codigo"]; // pegar o codigo da url
+
+            echo $_codig;
+        ?>
+
+
     </body>
 </html>
