f43c7ce10f3daf987eb7f2594203b11a843fac9e
Anderson Macedo Cavalcanti <anderson.macedo.cavalcanti@gmail.com>
2021-02-28 14:41:49 -0300

    Links com parâmetros

---
+++ PHP/Unidade13-Linkscomparâmetros.php
@@ -0,0 +1,18 @@
+<html lang = "pt-BR">
+	 <head>
+		  <meta charset="UTF-8">
+		  <title>Unidade 7</title>
+
+	 </head>
+	 <body>
+
+	    <ul>
+		    <li><a href="Utilitarios-Unidade13/destino.php?codigo=0">Laranja</a></li>
+		    <li><a href="Utilitarios-Unidade13/destino.php?codigo=1">Maca</a></li>
+		    <li><a href="Utilitarios-Unidade13/destino.php?codigo=2">Abacate</a></li>
+	    </ul>
+
+
+
+	 </body>
+</html>
