537bdbff0d2d5e5fedd55750f0ecaa23005cef8c
Anderson Macedo Cavalcanti <anderson.macedo.cavalcanti@gmail.com>
2021-03-09 19:34:39 -0300

    verificacao com bd login e senha

--- PHP - Banco de dados BASICO/login.php
+++ PHP - Banco de dados BASICO/login.php
@@ -1,5 +1,30 @@
 <?php require_once("../PHP - Banco de dados BASICO/conexao/conexao.php");
 
+	if( $_POST['email'] != null && $_POST['password'] != null){
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+
+        $consulta = "SELECT EMAIL, PASSWORD FROM LOGIN";
+        $consulta .= " WHERE EMAIL = '{$email}'";
+        $consulta .= " and PASSWORD = '{$password}'";
+
+        if (!empty($conecta)) {
+            $acesso = mysqli_query($conecta,$consulta);
+            if(!$acesso){
+            	die("Falha na consulta ao banco de dados");
+            }
+
+            $informacoes = mysqli_fetch_assoc($acesso);
+
+            if(empty($informacoes)){
+            	$mensagem = "Login sem sucesso";
+            } else{
+            	Header("Location: Unidade04-ListagemdeProdutos.php");
+            }
+}
+	}
+
+
 ?>
 
 
@@ -23,9 +48,10 @@
 			<form action="login.php" method="post">
 				<label for="text">E-mail</label>
 				<input type="email" name="email" id="email" placeholder="Digite seu e-mail" required="true">
-
+				<?php if (isset($mensagem)){?> <div><p class="erroLogin">* Verifique o E-mail digitado</p></div><?php }?>
 				<label for="password">Senha</label>
 				<input type="password" name="password" id="password" placeholder="Digite sua senha" required="true">
+                <?php if (isset($mensagem)){?> <div><p class="erroLogin">* Verifique a senha digitada</p></div><?php }?>
 				<section class="container-btn">
 					<button class="btn" id="btn-submit">
 						<p>Entrar</p>
